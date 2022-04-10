c4efc2e0010b5892432c172ad88d8e19926400aa
chhhloeeee <isabelchloe2@gmail.com>
2022-04-10 20:27:46 +0100

    unable to run application.java

--- loan/loan/src/main/java/com/example/loan/application.java
+++ loan/loan/src/main/java/com/example/loan/application.java
@@ -1,8 +1,11 @@
 package com.example.loan;
 
+import java.util.Arrays;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
  
 @SpringBootApplication
@@ -12,10 +15,19 @@ public class application {
         SpringApplication.run(application.class, args);
     }
 
-    @Bean(name=“entityManagerFactory”)
-        public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        return sessionFactory;
-    }
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
  
 }
