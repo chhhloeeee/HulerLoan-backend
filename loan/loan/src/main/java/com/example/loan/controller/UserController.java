68fbc64dae6f9fb4dce12522c2e8cbb5b2fe368b
chhhloeeee <isabelchloe2@gmail.com>
2022-05-05 08:23:38 +0100

    remove weird user shiz

--- loan/loan/src/main/java/com/example/loan/controller/UserController.java
+++ loan/loan/src/main/java/com/example/loan/controller/UserController.java
@@ -2,21 +2,16 @@ package com.example.loan.controller;
 
 import com.example.loan.model.User;
 import com.example.loan.service.UserService;
-import com.example.loan.Status;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 import java.util.HashMap;
 import java.util.List;
@@ -59,41 +54,4 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("/users/{userID}")
-    public ResponseEntity<User> updateUser(@PathVariable("userID") Integer userID,
-            @RequestBody User user) {
-        user = userService.updateUser(userID, user);
-        return ResponseEntity.ok(user);
-    }
-
-    @CrossOrigin()
-    @PostMapping("/users/login")
-    public ResponseEntity<User> loginUser(@Valid @RequestBody User user) {
-        List<User> users = userService.getAllUsers();
-
-        for (User other : users) {
-            if (other.equals(user)) {
-                user.setLoggedIn(true);
-                userService.saveUser(user);
-                return ResponseEntity.ok(user);
-            }
-        }
-        return ResponseEntity.ok(user);
-    }
-
-    @CrossOrigin()
-    @PostMapping("/users/logout")
-    public Status logUserOut(@Validated @RequestBody User user) {
-        List<User> users = userService.getAllUsers();
-
-        for (User other : users) {
-            if (other.equals(user)) {
-                user.setLoggedIn(false);
-                userService.saveUser(user);
-                return Status.SUCCESS;
-            }
-        }
-
-        return Status.FAILURE;
-    }
 }
